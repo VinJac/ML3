60833e0c0c5690539bcc5c7b5c17834bbd194d96
Vincent Jacob <vincent.jacob@edu.ece.fr>
2017-12-16 14:10:01 +0100

    Base de données simplifiée

--- database.sql
+++ database.sql
@@ -7,15 +7,8 @@ CREATE TABLE Gare (
     PRIMARY KEY (nomGare)
 );
 
-CREATE TABLE Ligne (
-	numeroLigne INT UNSIGNED,
-    PRIMARY KEY (numeroLigne)
-);
-
 CREATE TABLE Train (
 	numeroTrain INT UNSIGNED,
-	numeroLigne INT UNSIGNED NOT NULL,
-	sensParcours ENUM('ASC', 'DESC') NOT NULL,			-- ASC = Parcours tel qu'est déclaré la ligne / DESC = Parcours dans l'autre sens
     PRIMARY KEY (numeroTrain),
     FOREIGN KEY (numeroLigne) REFERENCES Ligne(numeroLigne) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -34,19 +27,10 @@ CREATE TABLE Train_Segment (
 	gareDepart VARCHAR(30),
 	gareArrivee VARCHAR(30),
 	vitesse REAL UNSIGNED NOT NULL,						-- En km/h
+	rang INT UNSIGNED NOT NULL,							-- Rang du segment dans le parcourt du train (de 1 au nombre de segments de la ligne)
 	FOREIGN KEY (numeroTrain) REFERENCES Train(numeroTrain) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (gareDepart, gareArrivee) REFERENCES Segment(gareDepart, gareArrivee) ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY (numeroTrain, gareDepart, gareArrivee)
-);
-
-CREATE TABLE Ligne_Segment (
-	numeroLigne INT UNSIGNED,
-	gareDepart VARCHAR(30),
-	gareArrivee VARCHAR(30),
-	rang INT UNSIGNED NOT NULL,						-- Rang du segment dans la ligne (de 1 au nombre de segments de la ligne)
-	FOREIGN KEY (numeroLigne) REFERENCES Ligne(numeroLigne) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (gareDepart, gareArrivee) REFERENCES Segment(gareDepart, gareArrivee) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (numeroLigne, gareDepart, gareArrivee)
 );
 
 CREATE TABLE Periode (
@@ -133,10 +117,6 @@ INSERT INTO Gare VALUES
     ('Marseille'),
     ('Strasbourg'),
     ('Montpellier');
-    
-INSERT INTO Ligne VALUES
-	(1),
-    (2);
 
 INSERT INTO Segment VALUES
 	('Paris', 'Lyon', 465),							-- 465km entre Paris et Lyon
@@ -144,32 +124,24 @@ INSERT INTO Segment VALUES
     ('Avignon', 'Marseille', 105),							
     ('Strasbourg', 'Lyon', 495),							
     ('Avignon', 'Montpellier', 95);
-	
-INSERT INTO Ligne_Segment VALUES
-	(1, 'Paris', 'Lyon', 1),
-    (1, 'Lyon', 'Avignon', 2),
-    (1, 'Avignon', 'Marseille', 3),
-	(2, 'Strasbourg', 'Lyon', 1),
-    (2, 'Lyon', 'Avignon', 2),
-    (2, 'Avignon', 'Montpellier', 3);
 
 INSERT INTO Train VALUES
-	(6607, 1, 'ASC'),								-- Car au départ de Paris
-    (6650, 2, 'ASC'),			
-    (6632, 1, 'DESC');								
+	(6607),											
+    (6650),			
+    (6632);								
 	
 INSERT INTO Train_Segment VALUES
-	(6607, 'Paris', 'Lyon', 310.00),					
-    (6607, 'Lyon', 'Avignon', 153.33),					
-    (6607, 'Avignon', 'Marseille', 210.00),				
+	(6607, 'Paris', 'Lyon', 310.00, 1),					
+    (6607, 'Lyon', 'Avignon', 153.33, 2),					
+    (6607, 'Avignon', 'Marseille', 210.00, 3),				
 
-	(6650, 'Strasbourg', 'Lyon', 141.43),				
-    (6650, 'Lyon', 'Avignon', 138.00),					
-    (6650, 'Avignon', 'Montpellier', 95.00),			
+	(6650, 'Strasbourg', 'Lyon', 141.43, 1),				
+    (6650, 'Lyon', 'Avignon', 138.00, 2),					
+    (6650, 'Avignon', 'Montpellier', 95.00, 3),			
 
-	(6632, 'Marseille', 'Avignon', 157.50),				
-    (6632, 'Avignon', 'Lyon', 138.00),						
-    (6632, 'Lyon', 'Paris', 174.38);					
+	(6632, 'Marseille', 'Avignon', 157.50, 1),				
+    (6632, 'Avignon', 'Lyon', 138.00, 2),						
+    (6632, 'Lyon', 'Paris', 174.38, 3);					
     
 INSERT INTO Periode VALUES
 	('bleue', 0.8),								
